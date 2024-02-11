7a77d6b66ed3ebd777cd365944e64eafc60d5704
IkemotoNanako <chibencaizi@gmail.com>
2024-02-11 19:56:36 +0900

    feature: mixin使用

--- lib/other/green_page.dart
+++ lib/other/green_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/weather/weather_page.dart';
 
-class GreenPage extends StatefulWidget {
-  const GreenPage({super.key});
+mixin afterFirstLayoutMixin<T extends StatefulWidget> on State<T> {
+  Future<void> afterFirstLayout() async {}
 
-  @override
-  State<GreenPage> createState() => _GreenPageState();
-}
-
-class _GreenPageState extends State<GreenPage> {
   @override
   void initState() {
     super.initState();
@@ -16,12 +11,30 @@ class _GreenPageState extends State<GreenPage> {
       await WidgetsBinding.instance.endOfFrame.then(
         (_) =>
             Future<void>.delayed(const Duration(milliseconds: 500)).then((_) {
-          Navigator.of(context).push(
-            MaterialPageRoute<void>(builder: (context) => const WeatherPage()),
-          );
+          afterFirstLayout();
         }),
       );
     });
+  }
+}
+
+class GreenPage extends StatefulWidget {
+  const GreenPage({super.key});
+
+  @override
+  State<GreenPage> createState() => _GreenPageState();
+}
+
+class _GreenPageState extends State<GreenPage> with afterFirstLayoutMixin {
+  @override
+  Future<void> afterFirstLayout() async {
+    await Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          return const WeatherPage();
+        },
+      ),
+    );
   }
 
   @override
